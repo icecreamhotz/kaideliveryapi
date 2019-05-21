b7c0d83fa058da0b4e1b3847ab3fb9cdb64eef7a
l3est <best_dota@hotmail.com>
2019-05-22 00:41:38 +0700

    initial commit

--- app/config/db_connection.js
+++ app/config/db_connection.js
@@ -13,6 +13,7 @@ connection.connect()*/
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize({
+  host: process.env.DATABASE_HOST,
   database: process.env.DATABASE_NAME,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
