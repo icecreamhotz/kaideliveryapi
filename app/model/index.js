0f42b2d017e2b3f3133086ab15c59b2e8b6050b1
l3est <best_dota@hotmail.com>
2019-05-22 00:43:35 +0700

    initial commit

--- app/model/index.js
+++ app/model/index.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize({
+  host: process.env.DATABASE_HOST,
   database: process.env.DATABASE_NAME,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
